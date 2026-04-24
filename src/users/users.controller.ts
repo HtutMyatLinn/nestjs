1bf410f05d76abef1135541095876a1d2a435010
htutmyatlinn <htutmyatlinnhml18@gmail.com>
2026-04-24 16:46:00 +0630

    pipe test

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,37 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  // @Get()
+  // public getUsers() {
+  //   return 'Get Users hit';
+  // }
+  @Get(':id')
+  public getUser(@Param('id', ParseIntPipe) id: number | undefined) {
+    console.log(id, typeof id);
+    return 'Get User hit';
+  }
   @Get()
-  public getUsers() {
-    return 'Get Users hit';
+  public getFilterUsers(
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: any,
+    @Query('offset', new DefaultValuePipe(8), ParseIntPipe) offset: any,
+  ) {
+    console.log(limit, offset);
+    return 'Get Filter Users hit';
   }
   @Post()
-  public updateUser() {
+  public updateUser(@Body() body: any) {
+    console.log(body);
     return 'Update User hit';
   }
   @Delete()
